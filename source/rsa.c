4e01570df275265f8c5b255fd137c5f90ceed61a
Indra Bagus Wicaksono <indra.bagus@gmail.com>
2013-08-28 14:02:12 +0700

    Implementasi fungsi public-private key generator ( belum ditest )

--- source/rsa.c
+++ source/rsa.c
@@ -1,32 +1,85 @@
 #include <gmp.h>
 #include <stdio.h>
+#include <windows.h>
 
-typedef struct internalrsa{
+#pragma comment(linker,"/NODEFAULTLIB:LIBC");
+
+#define PRIMESIZE   (BITSTRENGTH / 2)
+
+typedef struct internalrsa {
+    gmp_randstate_t hrandstate;
     mpz_t p;
     mpz_t q;
     mpz_t n;
     mpz_t z;
     mpz_t k;
+    mpz_t j;
 }privattrib_t,*PPRIVATEATTRIB;
 
 #define HANDLE2PRIVATTRIB(handle) ((PPRIVATEATTRIB)handle)
 
-static mpz_t hbigint;
+handle_t 
+rsa_initialize(void){
+    PPRIVATEATTRIB phandle = (PPRIVATEATTRIB)malloc(sizeof(privattrib_t));
+    gmp_randinit_default(phandle->hrandstate)
+    mpz_init2(phandle->p,PRIMESIZE);
+    mpz_init(phandle->q,PRIMESIZE);
+    mpz_init(phandle->n);
+    mpz_init(phandle->z);
+    mpz_init(phandle->k);
+    mpz_init(phandle->j);
+    return (handle_t)phandle;
+}
 
-/* --------- Mersenne Twister Start Here ------------ */
-static mpz_t MT[624];
-static int index;
+void 
+rsa_closehandle(handle_t h){
+    mpz_clear(HANDLE2PRIVATTRIB(h)->p);
+    mpz_clear(HANDLE2PRIVATTRIB(h)->q);
+    mpz_clear(HANDLE2PRIVATTRIB(h)->n);
+    mpz_clear(HANDLE2PRIVATTRIB(h)->z);
+    mpz_clear(HANDLE2PRIVATTRIB(h)->k);
+    mpz_clear(HANDLE2PRIVATTRIB(h)->j);
+    free(HANDLE2PRIVATTRIB(h));
+}
 
-
-
-void rsa_genkey(){
-/* pick two number (p and q ) */
-/* calculate n = p* q */
+int 
+rsa_createkey(handle_t handle,PRSAKEY_T key){
+    /* inisialisasi variabel helper */
+    mpz_t pminus;
+    mpz_t qminus;
+    mpz_t gcd;
+    unsigned long int k_int = 65537;
+    mpz_init(pminus);
+    mpz_init(qminus);
+    mpz_init(gcd);
+    /* pick two random prime number (p and q ) */
+    mpz_urandomb(HANDLE2PRIVATTRIB(handle)->p,HANDLE2PRIVATTRIB(handle)->hrandstate,PRIMESIZE);
+    mpz_urandomb(HANDLE2PRIVATTRIB(handle)->q,HANDLE2PRIVATTRIB(handle)->hrandstate,PRIMESIZE);
+    mpz_nextprime(HANDLE2PRIVATTRIB(handle)->q,HANDLE2PRIVATTRIB(handle)->q);
+    /* calculate n = (p * q) */
+    mpz_mul(HANDLE2PRIVATTRIB(handle)->n,HANDLE2PRIVATTRIB(handle)->q,HANDLE2PRIVATTRIB(handle)->p);
     /* calculate z = (p-1) * ( q - 1) */
+    mpz_sub_ui(pminus,HANDLE2PRIVATTRIB(handle)->p,(unsigned int)1);
+    mpz_sub_ui(qminus,HANDLE2PRIVATTRIB(handle)->q,(unsigned int)1);
+    mpz_mul(HANDLE2PRIVATTRIB(handle)->z,pminus,qminus);
     /* choose k, such that k is co-prime to z, i.e z is not divisible by k 
        or in other word gcd(k,z) = 1 */
+    while(1){
+        mpz_gcd_ui(gcd,HANDLE2PRIVATTRIB(handle)->z,k_int);
+        if(mpz_comp_ui(gcd,(unsigned long)1) == 0)
+            break;
+        k_int +=2;
+    }
+    mpz_set_ui(HANDLE2PRIVATTRIB(handle)->k,k_int);
     /* calculate j for ( k * j ) mod z = 1 */
+    if(mpz_invert(HANDLE2PRIVATTRIB(handle)->j,HANDLE2PRIVATTRIB(handle)->k,HANDLE2PRIVATTRIB(handle)->z) == 0){
+        /* cannot find j (multiplicative inverse) */
+        return -1;
+    }
     /* then we have publick key = [n,k] and private key [n,j] */
+    /* clear up everything */
+    mpz_clear(pminus);
+    mpz_clear(qminus);
 }
 
 void rsa_encrypt(Data P ){
--- source/rsa.h
+++ source/rsa.h
@@ -1,5 +1,10 @@
 #ifndef RSA_H
 #define RSA_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BITSTRENGTH 1024
 
 typedef struct rsa {
 
@@ -10,9 +15,13 @@ typedef rsakey_t* PRSAKEY;
 typedef void* handle_t;
 
 handle_t rsa_initialize(void);
-void rsa_createkey(handle_t handle,PRSAKEY_T key);
+int rsa_createkey(handle_t handle,PRSAKEY_T key);
 void rsa_encryptdata(const void* pdata, ulong_t length,void* pubuffer);
 void rsa_decryptdata(const void* pdata,ulong_t length,void* pbuffer);
-rsa_closehandle(handle_t h);
+void rsa_closehandle(handle_t h);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
