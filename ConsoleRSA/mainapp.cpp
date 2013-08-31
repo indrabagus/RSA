8b74426eee0047aa6263e5a1a120a3cf2a8a5357
Indra Bagus Wicaksono <indra.bagus@gmail.com>
2013-08-31 13:10:58 +0700

    Melengkapi aplikasi utama console untuk public/private key generation

--- ConsoleRSA/mainapp.cpp
+++ ConsoleRSA/mainapp.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <cassert>
+#include <rsa.h>
 
+static handle_t hrsa;
+static rsakey_t rsakey;
 int main(void)
 {
-    std::cout<<"hello you"<<std::endl;
+    std::cout<<"RSA initialization"<<std::endl;
+    hrsa = rsa_initialize();
+    assert(hrsa != 0);
+    int retval;
+    std::cout<<"RSA Key creation"<<std::endl;
+    do{
+        retval = rsa_createkey(hrsa,&rsakey);
+    }while(retval != 0);
+    std::cout<<"Public Key K: "<<rsakey.public_key.strkey_k<<std::endl;
+    std::cout<<"Public Key N: "<<rsakey.public_key.strkey_n<<std::endl<<std::endl<<std::endl;
+    std::cout<<"Private Key J: "<<rsakey.private_key.strkey_j<<std::endl<<std::endl;
+    std::cout<<"Private Key N: "<<rsakey.public_key.strkey_n<<std::endl<<std::endl;
+    std::cout<<"RSA close handle"<<std::endl;
+    rsa_closehandle(hrsa);
     return 0;
 }
